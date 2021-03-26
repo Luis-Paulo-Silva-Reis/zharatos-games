28ea51e86009839acc08009f4a1445961092c0b3
Luis Paulo Silva Reis <lpreis2010@gmail.com>
2021-03-26 14:03:26 -0300

    1.6

--- src/Componets/Pagination/Pagination.jsx
+++ src/Componets/Pagination/Pagination.jsx
@@ -52,7 +52,7 @@ export class FirstComponents extends PureComponent {
   // https://zharatosgames.com/db
   //"https://jsonplaceholder.typicode.com/posts"
   getData() {
-    axios.get("https://db.zharatosgames.com/data.json").then(res => {
+    axios.get("/data.json").then(res => {
       var data = res.data;
 
       var slice = data.slice(
